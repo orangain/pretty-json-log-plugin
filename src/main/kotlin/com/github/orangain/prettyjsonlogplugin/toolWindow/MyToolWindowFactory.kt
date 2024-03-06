2c187f60734e772d3b88dc24a2e348d05e6e9f79
GitHub Action <action@github.com>
2024-03-06 13:32:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/orangain/prettyjsonlogplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/orangain/prettyjsonlogplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.orangain.prettyjsonlogplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.orangain.prettyjsonlogplugin.MyBundle
+import com.github.orangain.prettyjsonlogplugin.services.MyProjectService
 import javax.swing.JButton
 
 
