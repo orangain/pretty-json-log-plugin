313f73e5b90281783db456b3fc47ba7b1bd430fc
orangain <orangain@gmail.com>
2024-03-09 17:28:05 +0900

    Add example script

---
+++ example/Example.kts
@@ -0,0 +1,37 @@
+import java.time.Instant
+
+fun log(level: String, message: String, traceId: String) {
+    val timestamp = Instant.now().toString()
+    val pid = ProcessHandle.current().pid()
+    val thread = Thread.currentThread().name
+    val escapedMessage = message
+        .replace("\\", "\\\\")
+        .replace("\"", "\\\"")
+        .replace("\n", "\\n")
+        .replace("\r", "\\r")
+        .replace("\t", "\\t")
+        .replace("\b", "\\b")
+
+    val json =
+        """{"timestamp":"$timestamp","pid":$pid,"thread":"$thread","level":"$level","message":"$escapedMessage","traceId":"$traceId"}"""
+    println(json)
+}
+
+val trace1 = "0af7651916cd43dd8448eb211c80319c"
+log("INFO", "Starting request", trace1)
+log("DEBUG", "Processing request", trace1)
+log("WARN", "Request is too large", trace1)
+log("DEBUG", "Still processing request", trace1)
+log("INFO", "200 OK: GET /", trace1)
+
+Thread.sleep(200)
+
+val trace2 = "4bf92f3577b34da6a3ce929d0e0e4736"
+try {
+    log("INFO", "Starting request", trace2)
+    log("DEBUG", "Processing request", trace2)
+    throw RuntimeException("Something went wrong!")
+} catch (e: Exception) {
+    log("ERROR", e.stackTraceToString(), trace2)
+    log("INFO", "500 Internal Server Error: GET /", trace2)
+}
