dff33a59f86d80f66cb84173f636e7eab59e4611
orangain <orangain@gmail.com>
2024-03-09 14:43:35 +0900

    Refactor parsing Timestamp

--- src/main/kotlin/io/github/orangain/prettyjsonlog/Extract.kt
+++ src/main/kotlin/io/github/orangain/prettyjsonlog/Extract.kt
@@ -11,6 +11,7 @@ private val timestampKeys = listOf("timestamp", "time", "@timestamp")
 
 sealed interface Timestamp {
     fun format(zoneId: ZoneId, formatter: DateTimeFormatter): String
+
     data class Parsed(val value: Instant) : Timestamp {
         override fun format(zoneId: ZoneId, formatter: DateTimeFormatter): String {
             return value.atZone(zoneId).format(formatter)
@@ -20,6 +21,22 @@ sealed interface Timestamp {
     data class Fallback(val value: String) : Timestamp {
         override fun format(zoneId: ZoneId, formatter: DateTimeFormatter): String {
             return value
+        }
+    }
+
+    companion object {
+        fun fromEpochMilli(value: Long): Parsed {
+            return Parsed(Instant.ofEpochMilli(value))
+        }
+
+        fun fromString(value: String): Timestamp {
+            return try {
+                // Use OffsetDateTime.parse instead of Instant.parse because Instant.parse in JDK <= 11 does not support non-UTC offset like "-05:00".
+                // See: https://stackoverflow.com/questions/68217689/how-to-use-instant-java-class-to-parse-a-date-time-with-offset-from-utc/68221614#68221614
+                Parsed(OffsetDateTime.parse(value).toInstant())
+            } catch (e: DateTimeParseException) {
+                Fallback(value)
+            }
         }
     }
 }
@@ -68,16 +85,9 @@ fun extractTimestamp(node: JsonNode): Timestamp? {
             ?.let { node ->
                 if (node.isNumber) {
                     // We assume that the number is a Unix timestamp in milliseconds.
-                    Timestamp.Parsed(Instant.ofEpochMilli(node.asLong()))
+                    Timestamp.fromEpochMilli(node.asLong())
                 } else {
-                    val text = node.asText()
-                    try {
-                        // Use OffsetDateTime.parse instead of Instant.parse because Instant.parse in JDK <= 11 does not support non-UTC offset like "-05:00".
-                        // See: https://stackoverflow.com/questions/68217689/how-to-use-instant-java-class-to-parse-a-date-time-with-offset-from-utc/68221614#68221614
-                        Timestamp.Parsed(OffsetDateTime.parse(text).toInstant())
-                    } catch (e: DateTimeParseException) {
-                        Timestamp.Fallback(text)
-                    }
+                    Timestamp.fromString(node.asText())
                 }
             }
     }
