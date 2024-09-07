ef35721c5bcb50a3f8fbb3e102534fd0ebbe97cf
orangain <orangain@gmail.com>
2024-09-07 22:55:01 +0900

    Apply isEnabled state to MyConsoleFolding

--- src/main/kotlin/io/github/orangain/prettyjsonlog/console/MyConsoleFolding.kt
+++ src/main/kotlin/io/github/orangain/prettyjsonlog/console/MyConsoleFolding.kt
@@ -1,18 +1,37 @@
 package io.github.orangain.prettyjsonlog.console
 
 import com.intellij.execution.ConsoleFolding
+import com.intellij.execution.ui.ConsoleView
+import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import io.github.orangain.prettyjsonlog.json.isPartOfPrettyJson
+import io.github.orangain.prettyjsonlog.service.EphemeralStateService
 
 class MyConsoleFolding : ConsoleFolding() {
+    private var consoleView: ConsoleView? = null
+
     override fun getPlaceholderText(project: Project, lines: List<String>): String {
         return "{...}"
     }
 
     override fun shouldFoldLine(project: Project, line: String): Boolean {
         thisLogger().debug("shouldFoldLine: $line")
+        if (!isEnabled(project)) {
+            return false
+        }
         return isPartOfPrettyJson(line)
+    }
+
+    override fun isEnabledForConsole(consoleView: ConsoleView): Boolean {
+        this.consoleView = consoleView
+        return true
+    }
+
+    private fun isEnabled(project: Project): Boolean {
+        val service = project.service<EphemeralStateService>()
+        val consoleView = this.consoleView ?: return false
+        return service.isEnabled(consoleView)
     }
 
 //    override fun shouldBeAttachedToThePreviousLine(): Boolean {
