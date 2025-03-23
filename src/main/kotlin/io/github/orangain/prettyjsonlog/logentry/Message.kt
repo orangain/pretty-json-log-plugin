f31250cdfcc38e71677d09c44d5b944eefaf4773
Komal Singh <softreigns@gmailcom>
2025-03-22 21:29:49 -0700

    nested message

--- src/main/kotlin/io/github/orangain/prettyjsonlog/logentry/Message.kt
+++ src/main/kotlin/io/github/orangain/prettyjsonlog/logentry/Message.kt
@@ -2,8 +2,17 @@ package io.github.orangain.prettyjsonlog.logentry
 
 import com.fasterxml.jackson.databind.JsonNode
 
-private val messageKeys = listOf("message", "msg", "error.message", "@m", "RenderedMessage")
+//private val messageKeys = listOf("message", "msg", "error.message", "@m", "RenderedMessage")
+
+private val messageKeys:List<NodeExtractor> = listOf(
+    { it.get("message") },
+    { it.get("msg") },
+    { it.get("sMsg.msg") },
+    { it.get("error.message") },
+    { it.get("@m") },
+    { it.get("RenderedMessage") },
+)
 
 fun extractMessage(node: JsonNode): String? {
-    return messageKeys.firstNotNullOfOrNull { node.get(it) }?.asText()
+    return messageKeys.firstNotNullOfOrNull { it(node) }?.asText()
 }
