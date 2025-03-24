06f36c096062caf7d955de38e64ca9e08e89823f
Zsigmond-Attila Szász <szaszzsigmondattila@gmail.com>
2025-03-24 20:44:05 +0200

    Add logMessage as a message key # Conflicts: #	src/main/kotlin/io/github/orangain/prettyjsonlog/logentry/Message.kt

--- src/main/kotlin/io/github/orangain/prettyjsonlog/logentry/Message.kt
+++ src/main/kotlin/io/github/orangain/prettyjsonlog/logentry/Message.kt
@@ -2,7 +2,7 @@ package io.github.orangain.prettyjsonlog.logentry
 
 import com.fasterxml.jackson.databind.JsonNode
 
-private val messageKeys = listOf("message", "msg", "error.message", "@m", "RenderedMessage")
+private val messageKeys = listOf("message", "msg", "error.message", "@m", "RenderedMessage", "logMessage")
 private val messageExtractors = buildNodeExtractors(messageKeys)
 
 fun extractMessage(node: JsonNode): String? {
