545237f8c020660252742a29af86bdc682fde822
Komal Singh <softreigns@gmailcom>
2025-08-24 10:48:13 -0700

    added try catch to parse xml/josn parts from message.

--- src/main/kotlin/io/github/orangain/prettyjsonlog/console/MyConsoleInputFilterProvider.kt
+++ src/main/kotlin/io/github/orangain/prettyjsonlog/console/MyConsoleInputFilterProvider.kt
@@ -1,5 +1,6 @@
 package io.github.orangain.prettyjsonlog.console
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.JsonNodeFactory
 import com.intellij.execution.filters.ConsoleDependentInputFilterProvider
 import com.intellij.execution.filters.InputFilter
@@ -10,17 +11,14 @@ import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Pair
 import com.intellij.psi.search.GlobalSearchScope
-import io.github.orangain.prettyjsonlog.json.parseJson
-import io.github.orangain.prettyjsonlog.json.prettifyXml
-import io.github.orangain.prettyjsonlog.json.prettyPrintJson
 import io.github.orangain.prettyjsonlog.logentry.*
 import io.github.orangain.prettyjsonlog.service.EphemeralStateService
 import java.text.SimpleDateFormat
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.*
+import com.google.gson.Gson
 import io.github.orangain.prettyjsonlog.json.*
-import com.fasterxml.jackson.databind.JsonNode
 
 // We use ConsoleDependentInputFilterProvider instead of ConsoleInputFilterProvider because we need to access
 // ConsoleView and Project in the filter.
@@ -37,8 +35,6 @@ class MyConsoleInputFilterProvider : ConsoleDependentInputFilterProvider() {
 
 private val zoneId = ZoneId.systemDefault()
 private val timestampFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
-
-private val jsonPartPattern = Regex("""\{[^}]*\}""")
 private val xmlPartPattern = Regex("<\\?xml.*?\\?>\\s*<([a-zA-Z_][\\w\\-.]*)(?:\\s[^>]*)?>.*?</\\1>", RegexOption.DOT_MATCHES_ALL)
 
 class MyConsoleInputFilter(
@@ -55,27 +51,31 @@ class MyConsoleInputFilter(
         }
 
 
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
-        var defaultNode = JsonNodeFactory.instance.objectNode()
-        defaultNode = defaultNode.set("@timestamp", JsonNodeFactory.instance.textNode(dateFormat.format(Date())))
-        defaultNode = defaultNode.set("@level", JsonNodeFactory.instance.textNode("INFO"))
-        defaultNode = defaultNode.set("@message", JsonNodeFactory.instance.textNode(text))
-
-
         var logText = text
         var tooLarge: Boolean = false
         thisLogger().info("Log text length: ${text.length}, contentType: $contentType")
         if (text.length > (8000) && text.contains("@timestamp")) {
             thisLogger().warn("Log text is too large to parse: characters")
             tooLarge = true
+            var textMessage: String? = null
+            try {
+                textMessage = extractFieldsFromText(text)
+            } catch (e: Exception) {
+                thisLogger().warn("Failed to extract fields from text: $text", e)
+                textMessage = "Log too large to parse"
+            }
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
             val time = Regex("\"@timestamp\"\\s*:\\s*\"((?:[^\"\\\\]|\\\\.)*)\"").find(text)?.groups?.get(1)?.value ?: dateFormat.format(Date())
-            val level = Regex("\"level\"\\s*:\\s*\"((?:[^\"\\\\]|\\\\.)*)\"").find(text)?.groups?.get(1)?.value ?: "INFO"
-            val defaultText = """{"@timestamp": "${time}","level": "$level","message": "Log too large to parse"}"""
+            val level = Regex("\"level\"\\s*:\\s*\"((?:[^\"\\\\]|\\\\.)*)\"").find(text)?.groups?.get(1)?.value ?: "UNKNOWN"
+            val defaultText = """{"@timestamp": "${time}","level": "$level","message": "$textMessage"}"""
             logText = defaultText
+
         }
 
-        if (parseJson(logText) == null)
+        if (parseJson(logText) == null) {
             thisLogger().debug("Log text is not a valid JSON: $logText")
+            return null
+        }
 
         val (node, suffixWhitespaces) = parseJson(logText) ?: return null
         thisLogger().debug("Parsed JSON node: $node")
@@ -88,52 +88,53 @@ class MyConsoleInputFilter(
         // .trimEnd('\n') is necessary because of the following reasons:
         // - When stackTrace is null or empty, we don't want to add an extra newline.
         // - When stackTrace ends with a newline, trimming the last newline makes a folding marker look better.
-        val coloredMessage = if (!tooLarge) "$level: $message\n${stackTrace ?: ""}".trimEnd('\n')
-            else "$level: ${extractFieldsFromText(text)}".trimEnd('\n')
+        val coloredMessage = "$level: $message\n${stackTrace ?: ""}".trimEnd('\n')
 
         var xmlPrettyPrintString = ""
-        if (message != null) {
-            val xmlParts = xmlPartPattern.findAll(message)
-            for (item in xmlParts.iterator()) {
-                val xmlString = item.groups[0]?.value.toString()
-                val onelineXML = xmlString.replace("\n", "")
-                if (message != null) {
-                    message = message.replace(xmlString, onelineXML)
-                }
-                var xmlPString = prettifyXml(xmlString)
-                xmlPString = xmlPString.replace(Regex("\n[\\s]*\n"), "\n")
-                xmlPString = xmlPString.trimEnd('\n')
-                xmlPrettyPrintString +=  if (xmlPrettyPrintString.isEmpty()) xmlPString else "\n${xmlPString.trim()}"
-            }
-            if (xmlPrettyPrintString.isNotEmpty()) {
-                xmlPrettyPrintString = "\n${xmlPrettyPrintString.trim()}"
-            }
-        }
-
         var jsonPartsPrettyString = ""
-        if (!message.isNullOrEmpty()) {
-            val jsonElements = extractJsonElements(message)
-//            println("Found ${jsonElements.size} valid JSON element(s):")
-            jsonElements.forEach {
-                thisLogger().debug("Found valid JSON element: $it")
-                val jsonNode: JsonNode? = getJsonNode(it.toString())
-                if (jsonNode != null) {
-                    val jsonPString = prettyPrintJson(jsonNode)
-                    jsonPartsPrettyString += if (jsonPartsPrettyString.isEmpty()) jsonPString else "\n$jsonPString"
+        try {
+            if (message != null) {
+                val xmlParts = xmlPartPattern.findAll(message)
+                for (item in xmlParts.iterator()) {
+                    val xmlString = item.groups[0]?.value.toString()
+                    val onelineXML = xmlString.replace("\n", "")
+                    if (message != null) {
+                        message = message.replace(xmlString, onelineXML)
+                    }
+                    var xmlPString = prettifyXml(xmlString)
+                    xmlPString = xmlPString.replace(Regex("\n[\\s]*\n"), "\n")
+                    xmlPString = xmlPString.trimEnd('\n')
+                    xmlPrettyPrintString += if (xmlPrettyPrintString.isEmpty()) xmlPString else "\n${xmlPString.trim()}"
+                }
+                if (xmlPrettyPrintString.isNotEmpty()) {
+                    xmlPrettyPrintString = "\n${xmlPrettyPrintString.trim()}"
                 }
             }
-        }
-        if (jsonPartsPrettyString.isNotEmpty()) {
-            jsonPartsPrettyString = "\n${jsonPartsPrettyString.trim()}"
+
+            if (!message.isNullOrEmpty()) {
+                val jsonElements = extractJsonElements(message)
+                jsonElements.forEach {
+                    if (it.toString().contains(":")) {
+                        thisLogger().debug("Found valid JSON element: $it")
+                        val jsonNode: JsonNode? = getJsonNode(it.toString())
+                        if (jsonNode != null) {
+                            val jsonPString = prettyPrintJson(jsonNode)
+                            jsonPartsPrettyString += if (jsonPartsPrettyString.isEmpty()) jsonPString else "\n$jsonPString"
+                        }
+                    }
+                }
+            }
+
+            if (jsonPartsPrettyString.isNotEmpty()) {
+                jsonPartsPrettyString = "\n${jsonPartsPrettyString.trim()}"
+            }
+        } catch (e: Exception) {
+            thisLogger().warn("Failed to pretty print XML/JSON parts in the message: $message", e)
         }
 
         var prettyJsonString = prettyPrintJson(node)
         val jsonString = if (tooLarge) text else "$prettyJsonString$jsonPartsPrettyString$xmlPrettyPrintString"
 
-        prettyJsonString = ""
-        jsonPartsPrettyString = ""
-        xmlPrettyPrintString = ""
-        message = ""
         return mutableListOf(
             Pair("[${timestamp?.format(zoneId, timestampFormatter)}] ", contentType),
             Pair(coloredMessage, contentTypeOf(level, contentType)),
